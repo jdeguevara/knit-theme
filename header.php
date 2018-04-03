b8108f458cb04cb9c9fff8ce61a4b465350181b5
jorge <jorge@knitmarketing.com>
2018-04-02 18:05:09 -0700

    Merge

--- header.php
+++ header.php
@@ -14,45 +14,15 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=0">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site container">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'knit-theme' ); ?></a>
-
-	<header id="masthead" class="site-header row">
-		<div class="site-branding col-md-4">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$knit_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $knit_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $knit_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'knit-theme' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+<div id="page" class="site">
+</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- page-home.php
+++ page-home.php
@@ -4,3 +4,32 @@
  */
 
 get_header(); ?>
+
+
+<?php 
+
+
+	$page_title= get_field('page_title');
+	$image= get_field('banner_image')
+?>
+
+
+
+
+
+<div class="jumbotron" style=" background-image: url(<?php if( $image ): echo $image; endif; ?>);">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+		
+
+				<h1 class=" "><?php if( $page_title ): echo $page_title; endif; ?></h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div>
+	<h1 class=" "><?php if( $page_title ): echo $page_title; endif; ?></h1>
+
+</div>
